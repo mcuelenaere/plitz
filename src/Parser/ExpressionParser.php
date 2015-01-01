7d7e343121586f0105160ff15d36628aaa55af5b
Maurus Cuelenaere <mcuelenaere@gmail.com>
2015-01-01 23:26:22 +0100

    Fix precedence climbing algorithm in ExpressionParser + add parenthesis support

--- src/Parser/ExpressionParser.php
+++ src/Parser/ExpressionParser.php
@@ -72,22 +72,33 @@ class ExpressionParser
      */
     public function parseExpression()
     {
-        return $this->parseExpressionWithPrecedence();
+        return $this->parseExpressionWithPrecedence($this->parsePrimaryExpression(), 0);
     }
 
-    private function parseExpressionWithPrecedence($precedence = 0)
+    private function parseExpressionWithPrecedence($left, $precedence)
     {
-        $left = $this->parsePrimaryExpression();
-
         list($token, ) = $this->tokenStream->current();
         while (isset(self::$binaryTokens[$token]) && self::$precedences[$token] >= $precedence) {
+            $operatorToken = $token;
+            $operator = self::$binaryTokens[$token];
             $this->tokenStream->next();
 
-            $operator = self::$binaryTokens[$token];
-            $right = $this->parseExpression($precedence + self::$associativity[$token]);
-            $left = new Expressions\Binary($left, $right, $operator);
+            $right = $this->parsePrimaryExpression();
 
             list($token, ) = $this->tokenStream->current();
+            while (
+                isset(self::$binaryTokens[$token]) &&
+                (
+                    self::$precedences[$token] > self::$precedences[$operatorToken] ||
+                    (self::$associativity[$token] === self::RIGHT_ASSOCIATIVE && self::$precedences[$token] === self::$precedences[$operatorToken])
+                )
+            ) {
+                $right = $this->parseExpressionWithPrecedence($right, self::$precedences[$token]);
+
+                list($token, ) = $this->tokenStream->current();
+            }
+
+            $left = new Expressions\Binary($left, $right, $operator);
         }
 
         return $left;
@@ -101,20 +112,36 @@ class ExpressionParser
                 return $this->parseVariableOrMethodCallExpression();
             case Tokens::T_NOT:
                 $this->tokenStream->next();
-                return new Expressions\Unary($this->parseExpression(), Expressions\Unary::OPERATION_NOT);
+                return new Expressions\Unary($this->parsePrimaryExpression(), Expressions\Unary::OPERATION_NOT);
             case Tokens::T_MINUS:
                 $this->tokenStream->next();
-                return new Expressions\Unary($this->parseExpression(), Expressions\Unary::OPERATION_NEGATE);
+                return new Expressions\Unary($this->parsePrimaryExpression(), Expressions\Unary::OPERATION_NEGATE);
             case Tokens::T_PLUS:
                 $this->tokenStream->next();
-                return $this->parseExpression();
+                return $this->parsePrimaryExpression();
             case Tokens::T_BOOL:
             case Tokens::T_STRING:
             case Tokens::T_NUMBER:
                 $this->tokenStream->next();
                 return new Expressions\Scalar($value);
+            case Tokens::T_OPEN_PAREN:
+                // eat T_OPEN_PAREN
+                $this->tokenStream->next();
+
+                // parse expression within parens
+                $expression = $this->parseExpression();
+
+                list($token, ) = $this->tokenStream->current();
+                if ($token !== Tokens::T_CLOSE_PAREN) {
+                    throw ParseException::createInvalidTokenException([Tokens::T_CLOSE_PAREN], $this->tokenStream);
+                }
+
+                // eat T_CLOSE_PAREN
+                $this->tokenStream->next();
+
+                return $expression;
             default:
-                throw ParseException::createInvalidTokenException([Tokens::T_BOOL, Tokens::T_STRING, Tokens::T_NUMBER, Tokens::T_MINUS, Tokens::T_PLUS, Tokens::T_NOT], $this->tokenStream);
+                throw ParseException::createInvalidTokenException([Tokens::T_BOOL, Tokens::T_STRING, Tokens::T_NUMBER, Tokens::T_MINUS, Tokens::T_PLUS, Tokens::T_NOT, Tokens::T_OPEN_PAREN, Tokens::T_LITERAL], $this->tokenStream);
         }
     }
 
