d2c94c798f57d1f2fa70f6679dca7f518399a6a8
Maurus Cuelenaere <mcuelenaere@gmail.com>
2015-01-04 23:56:02 +0100

    Also add column number to ParseException message

--- src/Parser/ParseException.php
+++ src/Parser/ParseException.php
@@ -67,11 +67,11 @@ class ParseException extends \LogicException
         list($actualToken, ) = $tokenStream->current();
 
         if (count($expectedTokens) === 1) {
-            $message = sprintf("Expected %s, but got %s instead at %s:%d", $expectedTokens[0], $actualToken, $tokenStream->getSource(), $tokenStream->getLine());
+            $message = sprintf("Expected %s, but got %s instead at %s:%d:%d", $expectedTokens[0], $actualToken, $tokenStream->getSource(), $tokenStream->getLine(), $tokenStream->getColumn());
         } else if (count($expectedTokens) > 2) {
-            $message = sprintf("Expected %s or %s, but got %s instead at %s:%d", implode(', ', array_slice($expectedTokens, 0, count($expectedTokens) - 1)), $expectedTokens[count($expectedTokens) - 1], $actualToken, $tokenStream->getSource(), $tokenStream->getLine());
+            $message = sprintf("Expected %s or %s, but got %s instead at %s:%d:%d", implode(', ', array_slice($expectedTokens, 0, count($expectedTokens) - 1)), $expectedTokens[count($expectedTokens) - 1], $actualToken, $tokenStream->getSource(), $tokenStream->getLine(), $tokenStream->getColumn());
         } else {
-            $message = sprintf("Expected %s or %s, but got %s instead at %s:%d", $expectedTokens[0], $expectedTokens[1], $actualToken, $tokenStream->getSource(), $tokenStream->getLine());
+            $message = sprintf("Expected %s or %s, but got %s instead at %s:%d:%d", $expectedTokens[0], $expectedTokens[1], $actualToken, $tokenStream->getSource(), $tokenStream->getLine(), $tokenStream->getColumn());
         }
 
         return new ParseException($message, $tokenStream->getSource(), $tokenStream->getLine(), $tokenStream->getColumn());
