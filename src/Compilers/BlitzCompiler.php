9d79fa97a0233812d93618a931d9cd566ae7e4ff
Maurus Cuelenaere <mcuelenaere@gmail.com>
2015-06-28 19:56:35 +0200

    Add initial version of BlitzCompiler, which is the first building block of allowing templates to be formatted in a consistent way

---
+++ src/Compilers/BlitzCompiler.php
@@ -0,0 +1,143 @@
+<?php
+namespace Plitz\Compilers;
+
+use Plitz\Parser\Expression;
+use Plitz\Parser\Expressions;
+use Plitz\Parser\Visitor;
+
+class BlitzCompiler implements Visitor
+{
+    /**
+     * Output stream
+     * @var resource
+     */
+    protected $output;
+
+    /**
+     * @param resource $stream
+     */
+    public function __construct($stream)
+    {
+        $this->output = $stream;
+    }
+
+    public function startOfStream()
+    {
+        // do nothing
+    }
+
+    public function endOfStream()
+    {
+        // do nothing
+    }
+
+    public function raw($data)
+    {
+        fwrite($this->output, $data);
+    }
+
+    public function ifBlock(Expression $condition)
+    {
+        if ($condition instanceof Expressions\Unary && $condition->getOperation() === Expressions\Unary::OPERATION_NOT) {
+            fwrite($this->output, '{{ UNLESS ');
+            $condition = $condition->getExpression();
+        } else {
+            fwrite($this->output, '{{ IF ');
+        }
+        $this->expression($condition);
+        fwrite($this->output, ' }}');
+    }
+
+    public function elseBlock()
+    {
+        fwrite($this->output, '{{ ELSE }}');
+    }
+
+    public function elseIfBlock(Expression $condition)
+    {
+        fwrite($this->output, '{{ ELSE IF ');
+        $this->expression($condition);
+        fwrite($this->output, ' }}');
+    }
+
+    public function endIfBlock()
+    {
+        fwrite($this->output, '{{ END }}');
+    }
+
+    public function loopBlock(Expression $variable)
+    {
+        fwrite($this->output, '{{ BEGIN ');
+        $this->expression($variable);
+        fwrite($this->output, ' }}');
+    }
+
+    public function endLoopBlock()
+    {
+        fwrite($this->output, '{{ END }}');
+    }
+
+    public function printBlock(Expression $value)
+    {
+        fwrite($this->output, '{{ ');
+        $this->expression($value);
+        fwrite($this->output, ' }}');
+    }
+
+    public function comment($data)
+    {
+        fwrite($this->output, '/* ' . $data . ' */');
+    }
+
+    protected function canParensBeOmittedFor(Expression $expr)
+    {
+        return $expr instanceof Expressions\Scalar ||
+            $expr instanceof Expressions\MethodCall ||
+            $expr instanceof Expressions\Variable;
+    }
+
+    protected function escapeScalar($variable)
+    {
+        return var_export($variable, true);
+    }
+
+    protected function expression(Expression $expr)
+    {
+        if ($expr instanceof Expressions\Binary) {
+            fwrite($this->output, "(");
+            $this->expression($expr->getLeft());
+            fwrite($this->output, " " . $expr->getOperation() . " ");
+            $this->expression($expr->getRight());
+            fwrite($this->output, ")");
+        } else if ($expr instanceof Expressions\Unary) {
+            $showParens = !$this->canParensBeOmittedFor($expr->getExpression());
+            fwrite($this->output, $expr->getOperation());
+            if ($showParens) {
+                fwrite($this->output, "(");
+            }
+            $this->expression($expr->getExpression());
+            if ($showParens) {
+                fwrite($this->output, ")");
+            }
+        } else if ($expr instanceof Expressions\MethodCall) {
+            fwrite($this->output, $expr->getMethodName() . "(");
+            $arguments = $expr->getArguments();
+            for ($i=0; $i < count($arguments); $i++) {
+                $this->expression($arguments[$i]);
+                if ($i < count($arguments) - 1) {
+                    fwrite($this->output, ", ");
+                }
+            }
+            fwrite($this->output, ")");
+        } else if ($expr instanceof Expressions\GetAttribute) {
+            $this->expression($expr->getExpression());
+            fwrite($this->output, '.' . $expr->getAttributeName());
+        } else if ($expr instanceof Expressions\Variable) {
+            fwrite($this->output, $expr->getVariableName());
+        } else if ($expr instanceof Expressions\Scalar) {
+            fwrite($this->output, $this->escapeScalar($expr->getValue()));
+        } else {
+            throw new \LogicException("Unknown expression class " . get_class($expr));
+        }
+    }
+}
