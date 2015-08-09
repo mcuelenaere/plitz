6f9f9fd7639b95f97de3579ff824ce65e4c953ab
Maurus Cuelenaere <mcuelenaere@gmail.com>
2015-08-09 13:13:11 +0200

    Also run integration tests with Plitz\Compilers\JsCompiler through Node.js

--- tests/Integration/PlitzIntegrationTest.php
+++ tests/Integration/PlitzIntegrationTest.php
@@ -1,6 +1,7 @@
 <?php
 namespace Plitz\Tests\Integration;
 
+use Plitz\Compilers\JsCompiler;
 use Plitz\Compilers\PhpCompiler;
 use Plitz\Lexer\Lexer;
 use Plitz\Parser\Parser;
@@ -23,17 +24,11 @@ class PlitzIntegrationTest extends \PHPUnit_Framework_TestCase
      */
     private $lexer;
 
-    /**
-     * @var PhpCompiler
-     */
-    private $compiler;
-
     protected function setUp()
     {
         $this->inputStream = fopen("php://temp", "r+");
         $this->outputStream = fopen("php://temp", "r+");
         $this->lexer = new Lexer($this->inputStream);
-        $this->compiler = new PhpCompiler($this->outputStream);
     }
 
     protected function tearDown()
@@ -61,18 +56,94 @@ class PlitzIntegrationTest extends \PHPUnit_Framework_TestCase
     }
 
     /**
+     * Helper function which returns the path to the Node.js binary
+     * @return string
+     */
+    private static function findNodeJsBinary()
+    {
+        static $binaries = ["node", "nodejs", "node.exe"];
+
+        foreach ($binaries as $binary) {
+            foreach (explode(PATH_SEPARATOR, getenv("PATH")) as $path) {
+                $filename = $path . DIRECTORY_SEPARATOR . $binary;
+
+                if (is_executable($filename)) {
+                    return $filename;
+                }
+            }
+        }
+
+        return null;
+    }
+
+    /**
+     * @param string $nodeJsPath
+     * @param string $jsCode
+     * @param array $context
+     * @return string
+     */
+    protected function executeJsCode($nodeJsPath, $jsCode, array $context)
+    {
+        $descriptorSpec = [
+            0 => ['pipe', 'r'],
+            1 => ['pipe', 'w'],
+            2 => ['pipe', 'w'],
+        ];
+
+        $p = proc_open($nodeJsPath, $descriptorSpec, $pipes);
+        if (!is_resource($p)) {
+            throw new \RuntimeException("Could not start Node.js process");
+        }
+
+        try {
+            stream_set_blocking($pipes[1], 0);
+            stream_set_blocking($pipes[2], 0);
+
+            // write JS code to process
+            fwrite($pipes[0], "var templateFn = " . $jsCode . ";\n");
+            fwrite($pipes[0], "var context = " . json_encode($context) . ";\n");
+            fwrite($pipes[0], "process.stdout.write(templateFn({}, context));\n");
+            fclose($pipes[0]);
+
+            // read output into memory buffers
+            $stdout = '';
+            $stderr = '';
+            while (proc_get_status($p)['running']) {
+                $fds = ['stdout' => $pipes[1], 'stderr' => $pipes[2]];
+                $null = null;
+                if (stream_select($fds, $null, $null, null, null) == 0) {
+                    continue;
+                }
+
+                foreach ($fds as $name => $fd) {
+                    ${$name} .= fread($fd, 8192);
+                }
+            }
+
+            if (!empty($stderr)) {
+                throw new \RuntimeException("Node.js gave an error: " . $stderr);
+            }
+
+            return $stdout;
+        } finally {
+            proc_close($p);
+        }
+    }
+
+    /**
      * @dataProvider provideYamlCases
      * @param string $template
      * @param array $assignments
      * @param string $expectedOutput
      */
-    public function testYamlCases($template, array $assignments, $expectedOutput)
+    public function testYamlCasesForPhp($template, array $assignments, $expectedOutput)
     {
         // write template to input stream
         fwrite($this->inputStream, $template);
         fseek($this->inputStream, 0, SEEK_SET);
 
-        $parser = new Parser($this->lexer->lex(), $this->compiler);
+        $compiler = new PhpCompiler($this->outputStream);
+        $parser = new Parser($this->lexer->lex(), $compiler);
         $parser->parse();
 
         // rewind output stream and read its contents
@@ -81,6 +152,37 @@ class PlitzIntegrationTest extends \PHPUnit_Framework_TestCase
 
         // evaluate generated PHP code
         $output = $this->executePhpCode($phpCode, $assignments);
+
+        $this->assertEquals($expectedOutput, $output);
+    }
+
+    /**
+     * @dataProvider provideYamlCases
+     * @param string $template
+     * @param array $assignments
+     * @param string $expectedOutput
+     */
+    public function testYamlCasesForJs($template, array $assignments, $expectedOutput)
+    {
+        $nodeJsPath = self::findNodeJsBinary();
+        if ($nodeJsPath === null) {
+            $this->markTestSkipped("Could not find Node.js binary");
+        }
+
+        // write template to input stream
+        fwrite($this->inputStream, $template);
+        fseek($this->inputStream, 0, SEEK_SET);
+
+        $compiler = new JsCompiler($this->outputStream);
+        $parser = new Parser($this->lexer->lex(), $compiler);
+        $parser->parse();
+
+        // rewind output stream and read its contents
+        fseek($this->outputStream, 0, SEEK_SET);
+        $phpCode = stream_get_contents($this->outputStream);
+
+        // evaluate generated JavaScript code
+        $output = $this->executeJsCode($nodeJsPath, $phpCode, $assignments);
 
         $this->assertEquals($expectedOutput, $output);
     }
